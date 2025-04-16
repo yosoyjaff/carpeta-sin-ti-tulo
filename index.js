2f7a48c409e8ef9bee2e454651332dc34f642c2c
Josue Ferreira <josuealejandroferreira@gmail.com>
2025-04-16 11:51:54 -0400

    test

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = 3441;
 
 const minioClient = new Minio.Client({
 	endPoint: process.env.MINIO_ENDPOINT,
-	port: parseInt(process.env.MINIO_PORT),
 	useSSL: process.env.MINIO_SSL === 'true',
 	accessKey: process.env.MINIO_ACCESS_KEY,
 	secretKey: process.env.MINIO_SECRET_KEY,
